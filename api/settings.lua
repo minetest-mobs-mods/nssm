75c89977e165982627bc0e21427fd66d89b1cba1
Tai Kedzierski <dch.tai@gmail.com>
2018-12-30 19:24:57 +0000

    fix setting for inhibition

--- api/settings.lua
+++ api/settings.lua
@@ -4,7 +4,7 @@ nssm.multimobs = tonumber(minetest.settings:get('nssm.multimobs')) or 1000
 -- Server safe setting - allow a non-griefing rainbow tool
 nssm.server_rainbow_staff = minetest.settings:get_bool('nssm.server_rainbow_staff', false)
 
-nssm.inhibition_radius = tonumber(minetest.settings:get('nssm.multimobs')) or 8
+nssm.inhibition_radius = tonumber(minetest.settings:get('nssm.inhibition_radius')) or 8
 
 nssm.energy_boosts = minetest.settings:get_bool('nssm.energy_boosts')
 
